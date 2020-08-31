b2a437003ea859eb1a6cc0ad29cc2afa5c1188f9
Philip Ng Pack <pngpack@icloud.com>
2020-08-30 23:18:34 -0400

    Added comments to functions

--- FlexSchedule/Supporting Views/CourseCards.swift
+++ FlexSchedule/Supporting Views/CourseCards.swift
@@ -13,39 +13,53 @@ struct CourseCards: View {
     @State var courseArray: [AnyView] = []
     
     var body: some View {
-        Text("hi")
-//        VStack {
-//            ForEach(0..<courseArray.count) { course in
-//                courseArray[course]
-//            }
+        
+        VStack {
+            Button(action: {
+                self.addCourse()
+                
+                
+            }) {
+                Image(systemName: "arrow.right.circle.fill")
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .foregroundColor(Color.gray)
+            }
         }
+        }
+        
+        
+        // this function should append to the array
+        func addCourse() {
+            courseArray.append(createCourseCard())
+        }
+        
+        // this function should remove a course from the array
+        func removeCourse() {
+//            button
+//            courseArray.remove(at: <#T##Int#>)
+        }
+        
+        // this function should create course and return a courseCard view
+        func createCourseCard() -> AnyView {
+            return VStack {
+                Spacer()
+                Text("Chemistry")
+                    .padding(10)
+                    .frame(minWidth: 0, maxWidth: 350)
+                    .background(Color.white)
+            }
+            .frame(height: 120)
+            .background(Color.green)
+            .cornerRadius(10)
+            .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.black.opacity(0.2), lineWidth: 1)) as! AnyView
+        }
+    
     }
     
-    // this function should append to the array
-    private func addCourse() {
-    }
-
-    // this function should remove a course from the array
-    private func removeCourse() {
-    }
-
-    // this function should create course and return a courseCard view
-    private func createCourseCard() -> AnyView {
-        return VStack {
-            Spacer()
-            Text("Chemistry")
-                .padding(10)
-                .frame(minWidth: 0, maxWidth: 350)
-                .background(Color.white)
+    struct CourseCards_Previews: PreviewProvider {
+        static var previews: some View {
+            CourseCards()
         }
-        .frame(height: 120)
-        .background(Color.green)
-        .cornerRadius(10)
-        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.black.opacity(0.2), lineWidth: 1)) as! AnyView
-    }
-    
-struct CourseCards_Previews: PreviewProvider {
-    static var previews: some View {
-        CourseCards()
-    }
 }
+
